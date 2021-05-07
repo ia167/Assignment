b8f6e83aee0dd4038c099cae9b6210ff9b09ddb0
ia167 <molly-williams27@hotmail.co.uk>
2021-05-07 13:20:04 +0100

    Change of script to set up environment as egm722. Files imported

--- GMScript.py
+++ GMScript.py
@@ -2,12 +2,33 @@
 # The purpose of this fused image is to make a 'Green Map' displaying three height bands of vegetation
 
 # Modules to import
-
 import rasterio as rio
+from rasterio.plot import show
 
 # Data to import
 DTM = rio.open('C:\GIS_Ulster\Programming\Assignment\Data\DTM.tif')
 DSM = rio.open('C:\GIS_Ulster\Programming\Assignment\Data\DSM.tif')
-Ortho = rio.open('C:\GIS_Ulster\Programming\Assignment\Data\Ortho.tif')
+Band1 = rio.open('C:\GIS_Ulster\Programming\Assignment\Data\Band1.tif')
+Band4 = rio.open('C:\GIS_Ulster\Programming\Assignment\Data\Band4.tif')
 
 print('image size (width, height): {} x {}'.format(DTM.width, DTM.height))
+show(DTM)
+show(DSM)
+show(Band1)
+show(Band4)
+
+# Step one: Combine DTM and DSM by subtraction to create nDSM
+
+
+# Step two: Reclassify nDSM
+
+# Step three: Calculate NDVI from Orthophoto Bands 1 and 4
+
+# Step four: Reclassify NDVI
+
+# Step five: Fuse nDSM and NDVI
+
+# Step six: Display final fused image
+
+
+
