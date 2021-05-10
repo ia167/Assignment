3c3ef22ab06a6384b250c9b04cb7e5118a008170
ia167 <molly-williams27@hotmail.co.uk>
2021-05-10 09:10:24 +0100

    Calculation of NDVI (Step 3)

--- GMScript.py
+++ GMScript.py
@@ -4,19 +4,26 @@
 # Modules to import
 import rasterio as rio
 from rasterio.plot import show
+import numpy as np
 
 # Data to import
 DTM = rio.open('C:\GIS_Ulster\Programming\Assignment\Data\DTM.tif')
 DSM = rio.open('C:\GIS_Ulster\Programming\Assignment\Data\DSM.tif')
-Band1 = rio.open('C:\GIS_Ulster\Programming\Assignment\Data\Band1.tif')
-Band4 = rio.open('C:\GIS_Ulster\Programming\Assignment\Data\Band4.tif')
+Band1 = rio.open('C:\GIS_Ulster\Programming\Assignment\Data\Band1.tif') # Band 1 of Orthophoto
+Band4 = rio.open('C:\GIS_Ulster\Programming\Assignment\Data\Band4.tif') # Band 4 of Orthophoto (NIR band)
+Ortho = rio.open ('C:\GIS_Ulster\Programming\Assignment\Data\Ortho.tif')
 
-print('image size (width, height): {} x {}'.format(DTM.width, DTM.height))
-show(DTM)
-show(DSM)
-show(Band1)
-show(Band4)
+# Display DTM image to visualise the area we are working with. Other files cover same area.
+# show(DTM)
+print('Coordinate Reference System:', DTM.crs)
 
+full_Ortho = Ortho.read()
+num_bands = Ortho.count
+print('Number of bands in image =', num_bands)
+Ortho_Band1 = Ortho.read(1) # Separates out band 1 (red band) from Ortho into its own array
+Ortho_Band4 = Ortho.read(4) # Separares out band 4 (NIR band) from Ortho into its own array
+
+# Now processing of data begins to produce fused image
 # Step one: Combine DTM and DSM by subtraction to create nDSM
 
 
@@ -24,11 +31,13 @@ show(Band4)
 
 # Step three: Calculate NDVI from Orthophoto Bands 1 and 4
 
+redBand = Ortho_Band1.astype('f4')
+nirBand = Ortho_Band4.astype('f4')
+NDVI = (nirBand-redBand)/(nirBand+redBand)
+show(NDVI)
+
 # Step four: Reclassify NDVI
 
 # Step five: Fuse nDSM and NDVI
 
 # Step six: Display final fused image
-
-
-
